5f80d4df5530261db3511bf70b0472eaef6dfdf1
pocketberserker <pocketberserker@gmail.com>
2018-05-06 08:15:11 +0900

    rename connect method

--- sample_executor.ts
+++ sample_executor.ts
@@ -3,7 +3,7 @@ import USI from "./src/index"
 async function main() {
     console.log("connecting")
 
-    const apery = USI.connect2("C:\\Users\\defli\\Downloads\\syogi\\apery_sdt5\\bin\\apery_sdt5_bmi2.exe", [], {windowsHide: false});
+    const apery = USI.execFile("C:\\Users\\defli\\Downloads\\syogi\\apery_sdt5\\bin\\apery_sdt5_bmi2.exe", [], {windowsHide: false});
     
     await apery.init();
 
